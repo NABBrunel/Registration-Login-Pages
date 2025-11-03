56e72f101a4c3b002e8ca978384449a9bd25a979
NABBrunel <Nadine.Aburumman@brunel.ac.uk>
2025-11-03 10:11:27 +0000

    Refactor variable declarations to use let

--- Registration-Login-Source Code-Frontend/LoginValidation.js
+++ Registration-Login-Source Code-Frontend/LoginValidation.js
@@ -2,14 +2,14 @@
 //At the beginning, the form is empty, and therefore, not valid
 
 //Data to send
-var formData = {
+let formData = {
     email: "",
     password: "",
     remember: false
 }
 
 //This data will be read but not be sent as part of the form submission
-var formValid = false;
+let formValid = false;
 
 //Function to read the form
 function readForm(){
@@ -21,7 +21,7 @@ function readForm(){
 //Function to validate the form
 function validateForm() {
     formValid = false;
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if ((formData.email.length == 0) || (formData.password.length == 0)){
         alert("Please fill in both email and password.");
@@ -34,14 +34,14 @@ function validateForm() {
 
 //Function to write the Login success on the page
 function createNewParagraph(content){
-    var text = document.createTextNode(content);
-    var paragraph = document.createElement("p");
+    let text = document.createTextNode(content);
+    let paragraph = document.createElement("p");
     paragraph.appendChild(text);
     paragraph.style = "white-space: pre;";
     paragraph.id = "hiddenParagraph";
 
-    var element = document.getElementById("hiddenSection");
-    var existingParagraph = document.getElementById("hiddenParagraph");
+    let element = document.getElementById("hiddenSection");
+    let existingParagraph = document.getElementById("hiddenParagraph");
     element.replaceChild(paragraph, existingParagraph);
 }
 
@@ -51,7 +51,7 @@ function loginUser(){
     readForm();
     validateForm();
     if (formValid){
-        var formText = "Login successful for: " + formData.email + "\n";
+        let formText = "Login successful for: " + formData.email + "\n";
         formText += formData.remember ? "User chose to be remembered." : "User did not choose to be remembered.";
 
         console.log(formText);
